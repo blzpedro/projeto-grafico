804de6cb53ac68a06cd8f1d7671782a780d49e7d
Unknown <anderssonweslley123@gmail.com>
2019-08-14 11:28:03 -0300

    test shell_exec 2

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
 <?php
-   $var = shell_exec("git log -1 --pretty=format:'%h - %s (%ci)' --abbrev-commit `git merge-base local-dev dev`");
+   $var = shell_exec("git log -1 --pretty=format:'%h - %s (%ci)' --abbrev-commit `git merge-base projeto-grafico dev`");
    var_dump($var);
 ?>
