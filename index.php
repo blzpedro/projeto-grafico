6938022c637ae1fc344d3458e8da1fa2284ad8e8
Pedro Melo <phenriqmelo99@gmail.com>
2019-09-18 09:55:02 -0300

    a

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
         'colunas' => array(
             array(
                 'valor' => 70,
-                'quantidade' => 10,
+                'quantidade' => 8,
                 'taxa_desconto' => 20,
                 'taxa_juros' => 20,
             )
         ),  
-        'p_nome' => '1016.2',
+        'p_nome' => '2016.2',
         'mensalidade' => 912,
     );
     // echo '<br><div class="centro" style="background:none;"><a class="link btn" href="graph.php?'.http_build_query($dados).'" size="5">Redirecionar ao gráfico</a></div>';
